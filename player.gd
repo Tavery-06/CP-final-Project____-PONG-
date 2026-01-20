3c6152f32e57d8dcd881ab93e1f0c9ab0a9f1f17
Tavery-06 <tavery19.lavender@gmail.com>
2026-01-19 21:48:28 -0800

    fix key input for 2 players and 1 player

--- player.gd
+++ player.gd
@@ -2,16 +2,26 @@ extends StaticBody2D
 
 var win_height: int
 var p_height: int
+var scene_name: String
+
 
 func _ready() -> void:
 	win_height = get_viewport_rect().size.y
-	p_height = $ColorRect.get_size().y 
+	p_height = $ColorRect.get_size().y
+	scene_name = get_tree().current_scene.name
+
 
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_W):
-		position.y -= get_parent().PADDLE_SPEED * delta
-	elif Input.is_key_pressed(KEY_S):
-		position.y += get_parent().PADDLE_SPEED * delta
+	if scene_name == "TwoPlayer":
+		if Input.is_key_pressed(KEY_W):
+			position.y -= get_parent().PADDLE_SPEED * delta
+		elif Input.is_key_pressed(KEY_S):
+			position.y += get_parent().PADDLE_SPEED * delta
 
-	# Limit paddle movement to window
+	elif scene_name == "Main":
+		if Input.is_key_pressed(KEY_UP):
+			position.y -= get_parent().PADDLE_SPEED * delta
+		elif Input.is_key_pressed(KEY_DOWN):
+			position.y += get_parent().PADDLE_SPEED * delta
+
 	position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
--- startscreen.gd
+++ startscreen.gd
@@ -5,9 +5,11 @@ extends Node2D
 func _ready():
 	$"start button".connect("pressed", Callable(self, "_on_start_pressed"))
 	$"start button2".connect("pressed", Callable(self, "_on_start2_pressed"))
-
+	$"Tutorial button".connect("pressed", Callable(self, "_on_tutorial_pressed"))
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
 	
 func _on_start2_pressed():
+	get_tree().change_scene_to_file("res://two .player.tscn")
+func _on_tutorial_pressed():
 	get_tree().change_scene_to_file("res://two .player.tscn")
