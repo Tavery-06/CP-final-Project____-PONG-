33b3d2754a24e6180c047553a8015db96eee31b7
Tavery-06 <tavery19.lavender@gmail.com>
2026-01-20 16:50:00 -0800

    finish win screens

--- main.gd
+++ main.gd
@@ -17,6 +17,7 @@ func _on_score_left_body_entered(body: Node2D) -> void:
 	can_score = false
 	score[1] += 1
 	$"display/CPU score".text = str(score[1])
+	_on_win()
 	$balltimer.start()
 
 
@@ -27,8 +28,23 @@ func _on_score_right_body_entered(body: Node2D) -> void:
 	can_score = false
 	score[0] += 1
 	$"display/Player score".text = str(score[0])
+	_on_win()
 	$balltimer.start()
-	
-func _on_win():
-	if score[0] == 1:
+
+
+func _on_win() -> void:
+	if score[0] == 7:
+		await _win_delay()
 		get_tree().change_scene_to_file("res://winscreen_player_1.tscn")
+
+	elif score[1] == 7:
+		await _win_delay()
+		get_tree().change_scene_to_file("res://winscreen_cpu.tscn")
+
+
+func _win_delay() -> void:
+	# Optional: stop the ball so nothing weird happens
+	$ball.set_physics_process(false)
+
+	# 1-second delay
+	await get_tree().create_timer(1.0).timeout
